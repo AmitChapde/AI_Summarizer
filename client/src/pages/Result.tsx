81fe0cc18bd20fc5c73cfd259f909742fea7fcdc
Amit Chapde <amitchapde2@gmail.com>
2026-02-16 14:34:16 +0530

    refactor:css

--- client/src/pages/Result.tsx
+++ client/src/pages/Result.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import type { ResearchBrief } from "../types/research";
-import "../styles/global.css"; 
+
 
 export default function Result() {
   const location = useLocation();
